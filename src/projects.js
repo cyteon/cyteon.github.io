f1920874b4ea88fb2c193a216082782faa288bd7
cyteon <cyteon>
2024-09-14 22:04:26 +0200

    show project cards from a js file instead

---
+++ src/projects.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    title: "CatOS",
+    description: "An OS in Rust",
+    image: "public/catos.webp",
+    icons: [
+      "https://img.shields.io/badge/Rust-black?style=for-the-badge&logo=rust&logoColor=#E57324",
+    ],
+    links: [
+      {
+        label: "GitHub",
+        url: "https://github.com/cyteon/catos",
+      },
+    ],
+  },
+];
+
+var project_holder = document.getElementById("project_holder");
+
+projects.forEach((project) => {
+  var card = document.createElement("div");
+  card.className = "card";
+
+  var img = document.createElement("img");
+  img.width = 250;
+  img.height = 250;
+  img.alt = project.title + " image";
+  img.src = project.image;
+  card.append(img);
+
+  var title = document.createElement("h1");
+  title.innerHTML = project.title;
+  card.append(title);
+
+  var description = document.createElement("p");
+  description.innerHTML = project.description;
+  card.append(description);
+
+  var icons = document.createElement("div");
+  project.icons.forEach((icon) => {
+    var img = document.createElement("img");
+    img.src = icon;
+    icons.append(img);
+  });
+  card.append(icons);
+
+  var links = document.createElement("div");
+  project.links.forEach((linkData) => {
+    var link = document.createElement("a");
+    link.href = linkData.url;
+    link.innerHTML = linkData.label;
+    links.append(link);
+  });
+  card.append(links);
+
+  project_holder.append(card);
+});
