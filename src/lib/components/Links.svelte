2caea5b3a03cfe5004e8941a3c45761eda821591
cyteon <cyteon@hackclub.app>
2025-07-01 17:35:04 +0200

    add links

---
+++ src/lib/components/Links.svelte
@@ -0,0 +1,5 @@
+<div class="md:ml-auto text-xl md:mt-10 flex">
+    <a href="/">home</a>
+    <p class="mx-2 text-ctp-surface2">/</p>
+    <a href="/projects">projects</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Links from "$lib/components/Links.svelte";
+
+</script>
+
+<div class="px-2 md:w-3/5 mx-auto">
+    <div class="flex w-full flex-col md:flex-row">
+        <h1 class="text-transparent text-8xl leading-relaxed bg-gradient-to-r from-ctp-blue to-ctp-yellow bg-clip-text font-bold inline-block">
+            Cyteon
+        </h1>
+
+        <Links />
+    </div>
+</div>
